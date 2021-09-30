ba81caea0de35e0dd43b1f323812d324b0c4bd46
Yunohu <kernighan@gmail.com>
2021-09-30 20:10:33 +0000

    Create tools dir Move the perl note converter into it
    Add prototype for 3mle-archeage python conversion script

---
+++ tools/3mle-archeage.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+# Tool to convert songs made in 3MLE or imported
+# into 3MLE from a MIDI file for use in Archeage.
+
+# Volume conversion: 3mle = archeage
+# v0 = v0
+# v1 = v9
+# v2= v18
+# v3= v26
+# v4= v35
+# v5= v43
+# v6= v51
+# v7= v60
+# v8= v68
+# v9= v76
+# v10=v85
+# v11= v93
+# v12= v101
+# v13= v110
+# v14= v118
+# v15= v127
+
+# What needs to be done
+# Any "o" needs to be increased by one
+# Any "v" needs to be adjusted as above
+# Any "n" needs to be mapped back to a note with the appropriate octave change
+# If there are any instances of "l" being used, a new track must default back
+# to "l4" unless it starts with an "l" setting.
